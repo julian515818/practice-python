15454b08a51acd0ecfa263cb1b6b87710a5110e1
julian515818 <61299860+julian515818@users.noreply.github.com>
2020-02-24 18:01:48 +0800

    Create datatype01.py

---
+++ datatype01.py
@@ -0,0 +1,14 @@
+# encoding=utf-8
+# Programmer: 劉志俊
+# Date: 2020/02/17
+# Python 基本資料型態: int, float, str, bool
+
+myInt = 10
+myFloat = 3.1416
+myString = "Hello, World!"
+myBool = True
+
+print("整數", myInt)
+print("浮點數", myFloat)
+print("字串", myString)
+print("布林", myBool)
